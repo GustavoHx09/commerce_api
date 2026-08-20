34f60ec8b0bde2c86dca07fc18ad1005617bd5b9
GustavoHx09 <gustavo.henrique091104@gmail.com>
2026-08-19 21:43:31 -0300

    Refactory: MonoRepo, removed server.js

--- packages/api/src/app.js
+++ packages/api/src/app.js
@@ -1,8 +1,13 @@
 import express from "express";
 import routes from "./routes/index.js";
 import cors from "cors";
+import dotenv from "dotenv";
+import { connectDB } from "./config/connectDB.js";
+
+dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 3001;
 
 app.use(
     cors({
@@ -18,5 +23,11 @@ app.use(express.json());
 
 // aqui eu centralizo todas as rotas...
 app.use('/api/v1', routes);
+
+connectDB();
+
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+});
 
 export default app;
